a96a53c6bad3583913aa2ef84f2c12d2302f89cc
Anton <anton.loktev@stud.onu.edu.ua>
2018-12-17 00:14:32 +0200

    Added problem 3

--- task3.py
+++ task3.py
@@ -8,7 +8,7 @@ text1 = "How are you? Eh, ok. Low or Lower? Ohhh."
 
 
 def find_capital_letters(text):
-    return ''.join(c for c in text if c.isupper())
+    return "".join(c for c in text if c.isupper())
 
 
 print(find_capital_letters(text1))
@@ -21,9 +21,9 @@ print(find_capital_letters(text1))
 numbers = [341, 561, 645, 1105, 1387, 1729, 1905]
 
 
-def multiply_with_even_indexes(list):
-    sum_of_even_indexes = sum(x for x in list if list.index(x) % 2 == 0)
-    return sum_of_even_indexes * list[-1]
+def multiply_with_even_indexes(num_list):
+    sum_of_even_indexes = sum(x for x in num_list if num_list.index(x) % 2 == 0)
+    return sum_of_even_indexes * num_list[-1]
 
 
 print(multiply_with_even_indexes(numbers))
@@ -37,16 +37,36 @@ print(multiply_with_even_indexes(numbers))
 #   4)Весь текст может быть представлен только одним словом и все
 
 
+new_text = ". It's a text..."
+
+
+def without_dots_comas(text):
+    no_dots = text.replace(".", "")
+    return no_dots.replace(",", "")
+
+
+def find_word(text):
+    words = without_dots_comas(text).split()
+    result = []
+    for i in range(len(words)):
+        if words[i].isalpha() or words[i].count(chr(39)):
+            result.append(words[i])
+    return result[0] if len(result) else "There are no words :("
+
+
+print(find_word(new_text))
+
+
 # Задача-4
 # Изменить исходную строку на новую строку в которой первый и последний символы строки поменяны местами.
 
-text = 'python'
+text = "python"
 
 
 def swap_first_last(string):
     list_of_letters = list(string)
     list_of_letters[0], list_of_letters[-1] = list_of_letters[-1], list_of_letters[0]
-    return ''.join(list_of_letters)
+    return "".join(list_of_letters)
 
 
 print(swap_first_last(text))
@@ -58,31 +78,13 @@ print(swap_first_last(text))
 # Задача-5
 # Дан тапл(tuple), необходимо его конвертнуть в стринг.
 # Например:
-# ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's') == 'exercises
+# ("e", "x", "e", "r", "c", "i", "s", "e", "s") == 'exercises
 
-tuple_of_letters = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+tuple_of_letters = ("e", "x", "e", "r", "c", "i", "s", "e", "s")
 
 
 def convert_to_string(tup):
-    return ''.join(tup)
+    return "".join(tup)
 
 
 print(convert_to_string(tuple_of_letters))
-
-
-# **Задача-6 (Не обязательно, для тех кто скучает)
-# You would like to set a password for a bank account. However, there are three restrictions on the format
-# of the password:
-#  1) it has to contain only alphanumerical characters (a−z, A−Z, 0−9);
-#  2) there should be an even number of letters;
-#  3) there should be an odd number of digits.
-# You are given a string S consisting of N characters. String S can be divided into words by splitting it at,
-#  and removing, the spaces.
-# The goal is to choose the longest word that is a valid password.
-# You can assume that if there are K spaces in string S then there are exactly K + 1 words.
-#
-# For example, given "test 5 a0A pass007 ?xy1", there are five words and three of them are valid passwords: "5",
-#  "a0A" and "pass007".
-# Thus the longest password is "pass007" and its length is 7.
-# Note that neither "test" nor "?xy1" is a valid password, because "?" is not an alphanumerical character and
-#  "test" contains an even number of digits (zero).
