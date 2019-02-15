bddaa572caeadc58caedb7c13581557f8d12b7ad
antonloktev <antonios113loktev@gmail.com>
2019-02-15 13:04:56 +0200

    Updated problem 3 via @contextmanager

--- task8/problem3.py
+++ task8/problem3.py
@@ -19,3 +19,22 @@ class Timer(object):
 with Timer('Loop'):
     for i, k in enumerate(range(501)):
         print(i, k ** k)
+
+
+@contextmanager
+def timer(name='Some block'):
+    start = time.time()
+    try:
+        yield
+    except Exception as exc:
+        print(('There is an exception: {}').format(type(exc).__name__))
+    finally:
+        end = time.time()
+        execution_time = end - start
+        print(('Execution time for {} is: {}').format(name, execution_time))
+
+
+with timer('Just the same loop'):
+    for i, k in enumerate(range(501)):
+        print(i, k ** k)
+
